f40b0a0e68d030a5ed4ba26f1c66a6e05a0e65fb
wangrui <wangruivlife@gmail.com>
2021-02-25 22:43:34 +0800

    jvmTest代码

---
+++ daily_note/xmind/马士兵JVM/jvmTest/src/ClassloaderLevel.java
@@ -0,0 +1,11 @@
+public class ClassloaderLevel {
+    public static void main(String[] args) {
+        System.out.println(String.class.getClassLoader());
+        System.out.println(sun.awt.HKSCS.class.getClassLoader());
+        System.out.println(sun.net.spi.nameservice.dns.DNSNameService.class.getClassLoader());
+        System.out.println(ClassloaderLevel.class.getClassLoader());
+
+        System.out.println(sun.net.spi.nameservice.dns.DNSNameService.class.getClassLoader().getClass().getClassLoader());
+        System.out.println(ClassloaderLevel.class.getClassLoader().getClass().getClassLoader());
+    }
+}
