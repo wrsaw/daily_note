3fd916b4b875421876ad8978afd3c2cebfc253fa
wangrui <wangrui@vlifepaper.com>
2021-06-01 17:45:55 +0800

    13、JUC中的LockSupport工具类

---
+++ daily_note/concurrent/lurenjia/code/src/main/java/s13locksupport/LockSupport1.java
@@ -0,0 +1,26 @@
+package s13locksupport;
+
+import java.util.concurrent.locks.LockSupport;
+
+/**
+ * @author WangRui
+ * create at 2021/05/27 下午 3:48
+ **/
+public class LockSupport1 {
+    private static class T extends Thread {
+        @Override
+        public void run() {
+            System.out.println(System.currentTimeMillis() + ":开始执行");
+            LockSupport.park();
+            System.out.println(System.currentTimeMillis() + ":被唤醒");
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        T t = new T();
+        t.start();
+        Thread.sleep(5000);
+        LockSupport.unpark(t);
+        System.out.println(System.currentTimeMillis() + ":主线程执行完毕");
+    }
+}
