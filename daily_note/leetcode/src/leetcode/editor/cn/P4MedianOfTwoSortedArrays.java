c4206db5018f34e1e425225c25253a5b0d6349b8
wangrui <wangrui@vlifepaper.com>
2021-06-04 17:00:37 +0800

    两数组中位数-官方二分法

--- daily_note/leetcode/src/leetcode/editor/cn/P4MedianOfTwoSortedArrays.java
+++ daily_note/leetcode/src/leetcode/editor/cn/P4MedianOfTwoSortedArrays.java
@@ -70,8 +70,11 @@ public class P4MedianOfTwoSortedArrays{
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
         public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-            //TODO 二分查找
-            return 0;
+            if ((nums1.length + nums2.length) % 2 == 1) {
+                return getKthElement(nums1, nums2, (nums1.length + nums2.length) / 2 + 1);
+            } else {
+                return (getKthElement(nums1, nums2, (nums1.length + nums2.length) / 2) + getKthElement(nums1, nums2, (nums1.length + nums2.length) / 2 + 1)) / 2.0;
+            }
 
             //暴力解决
             /*int length = nums1.length + nums2.length;
@@ -124,7 +127,40 @@ public class P4MedianOfTwoSortedArrays{
             }
             return (double) (v1 + v2) / 2;*/
         }
+
+        private int getKthElement(int[] nums1, int[] nums2, int k) {
+            int length1 = nums1.length, length2 = nums2.length;
+            int index1 = 0, index2 = 0;
+
+            while (true) {
+                if (index1 == length1) {
+                    return nums2[index2 + k - 1];
+                }
+                if (index2 == length2) {
+                    return nums1[index1 + k - 1];
+                }
+                if (k == 1) {
+                    return Math.min(nums1[index1], nums2[index2]);
+                }
+
+                int half = k / 2;
+                int newIndex1 = Math.min(index1 + half, length1) - 1;
+                int newIndex2 = Math.min(index2 + half, length2) - 1;
+                int pivot1 = nums1[newIndex1], pivot2 = nums2[newIndex2];
+                if (pivot1 <= pivot2) {
+                    k -= (newIndex1 - index1 + 1);
+                    index1 = newIndex1 + 1;
+                } else {
+                    k -= (newIndex2 - index2 + 1);
+                    index2 = newIndex2 + 1;
+                }
+            }
+        }
+
     }
+
+
+
     //leetcode submit region end(Prohibit modification and deletion)
 
 }
