91d1fa855dd937d13cf1afa184ce9b157bae820c
wangrui <wangrui@vlifepaper.com>
2021-08-25 14:01:12 +0800

    所有可能的路径--回溯

---
+++ daily_note/leetcode/src/leetcode/editor/cn/P797AllPathsFromSourceToTarget.java
@@ -0,0 +1,100 @@
+package leetcode.editor.cn;
+
+//给你一个有 n 个节点的 有向无环图（DAG），请你找出所有从节点 0 到节点 n-1 的路径并输出（不要求按特定顺序） 
+//
+// 二维数组的第 i 个数组中的单元都表示有向图中 i 号节点所能到达的下一些节点，空就是没有下一个结点了。 
+//
+// 译者注：有向图是有方向的，即规定了 a→b 你就不能从 b→a 。 
+//
+// 
+//
+// 示例 1： 
+//
+// 
+//
+// 
+//输入：graph = [[1,2],[3],[3],[]]
+//输出：[[0,1,3],[0,2,3]]
+//解释：有两条路径 0 -> 1 -> 3 和 0 -> 2 -> 3
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入：graph = [[4,3,1],[3,2,4],[3],[4],[]]
+//输出：[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+// 
+//
+// 示例 3： 
+//
+// 
+//输入：graph = [[1],[]]
+//输出：[[0,1]]
+// 
+//
+// 示例 4： 
+//
+// 
+//输入：graph = [[1,2,3],[2],[3],[]]
+//输出：[[0,1,2,3],[0,2,3],[0,3]]
+// 
+//
+// 示例 5： 
+//
+// 
+//输入：graph = [[1,3],[2],[3],[]]
+//输出：[[0,1,2,3],[0,3]]
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// n == graph.length 
+// 2 <= n <= 15 
+// 0 <= graph[i][j] < n 
+// graph[i][j] != i（即，不存在自环） 
+// graph[i] 中的所有元素 互不相同 
+// 保证输入为 有向无环图（DAG） 
+// 
+// Related Topics 深度优先搜索 广度优先搜索 图 回溯 
+// 👍 166 👎 0
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class P797AllPathsFromSourceToTarget{
+    public static void main(String[] args) {
+        Solution solution = new P797AllPathsFromSourceToTarget().new Solution();
+        int[][] graph = new int[][]{{4, 3, 1}, {3, 2, 4}, {3}, {4}, {}};
+        System.out.println(solution.allPathsSourceTarget(graph));
+    }
+    //输出：[[0,4],[0,3,4],[0,1,3,4],[0,1,2,3,4],[0,1,4]]
+    //leetcode submit region begin(Prohibit modification and deletion)
+    class Solution {
+        public List<List<Integer>> allPathsSourceTarget(int[][] graph) {
+            List<List<Integer>> result = new ArrayList<>();
+            List<Integer> temp = new ArrayList<>();
+            temp.add(0);
+            handle(graph, graph[0], temp, result, graph.length - 1);
+            return result;
+        }
+
+        private void handle(int[][] graph, int[] graphNode, List<Integer> temp, List<List<Integer>> result, int n) {
+            for (int i : graphNode) {
+                temp.add(i);
+                if (i == n) {
+                    List<Integer> tempRes = new ArrayList<>(temp);
+                    result.add(tempRes);
+                }
+                handle(graph, graph[i], temp, result, n);
+                temp.remove(temp.size() - 1);
+            }
+        }
+    }
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
