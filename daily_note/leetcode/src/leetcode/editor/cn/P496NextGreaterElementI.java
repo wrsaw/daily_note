4cb2ae4b57f4d518a7821370de1e6a55ea053c4f
wangrui <wangrui@vlifepaper.com>
2021-10-26 11:05:53 +0800

    下一个更大元素--栈、哈希

---
+++ daily_note/leetcode/src/leetcode/editor/cn/P496NextGreaterElementI.java
@@ -0,0 +1,88 @@
+package leetcode.editor.cn;
+
+//给你两个 没有重复元素 的数组 nums1 和 nums2 ，其中nums1 是 nums2 的子集。 
+//
+// 请你找出 nums1 中每个元素在 nums2 中的下一个比其大的值。 
+//
+// nums1 中数字 x 的下一个更大元素是指 x 在 nums2 中对应位置的右边的第一个比 x 大的元素。如果不存在，对应位置输出 -1 。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//输入: nums1 = [4,1,2], nums2 = [1,3,4,2].
+//输出: [-1,3,-1]
+//解释:
+//    对于 num1 中的数字 4 ，你无法在第二个数组中找到下一个更大的数字，因此输出 -1 。
+//    对于 num1 中的数字 1 ，第二个数组中数字1右边的下一个较大数字是 3 。
+//    对于 num1 中的数字 2 ，第二个数组中没有下一个更大的数字，因此输出 -1 。 
+//
+// 示例 2: 
+//
+// 
+//输入: nums1 = [2,4], nums2 = [1,2,3,4].
+//输出: [3,-1]
+//解释:
+//    对于 num1 中的数字 2 ，第二个数组中的下一个较大数字是 3 。
+//    对于 num1 中的数字 4 ，第二个数组中没有下一个更大的数字，因此输出 -1 。
+// 
+//
+//
+//
+// 提示： 
+//
+// 
+// 1 <= nums1.length <= nums2.length <= 1000 
+// 0 <= nums1[i], nums2[i] <= 104 
+// nums1和nums2中所有整数 互不相同 
+// nums1 中的所有整数同样出现在 nums2 中 
+// 
+//
+// 
+//
+// 进阶：你可以设计一个时间复杂度为 O(nums1.length + nums2.length) 的解决方案吗？ 
+// Related Topics 栈 数组 哈希表 单调栈 
+// 👍 541 👎 0
+
+
+import java.util.Deque;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public class P496NextGreaterElementI{
+    public static void main(String[] args) {
+        Solution solution = new P496NextGreaterElementI().new Solution();
+        
+    }
+    //leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Deque<Integer> stack = new LinkedList<>();
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int num : nums2) {
+            if (stack.isEmpty()) {
+                stack.push(num);
+            } else if (num > stack.peek()) {
+                while (!stack.isEmpty() && num > stack.peek()) {
+                    map.put(stack.pop(), num);
+                }
+                stack.push(num);
+            } else {
+                stack.push(num);
+            }
+        }
+        while (!stack.isEmpty()) {
+            map.put(stack.pop(), -1);
+        }
+        int[] ans = new int[nums1.length];
+        for (int i = 0; i < nums1.length; i++) {
+            ans[i] = map.get(nums1[i]);
+        }
+        return ans;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
+
+}
